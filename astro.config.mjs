e3774a1c25d9cb040a72cd7955e58e4672d77695
bushdoggo <nociwokanto@gmail.com>
2025-01-05 17:38:21 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bushdoggo.github.io.',
+	base: '/bushdoggo.com',
 	integrations: [mdx(), sitemap()],
 });
