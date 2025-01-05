2aa311037e736af2bbc8e726ade4ba1c09747ac3
bushdoggo <nociwokanto@gmail.com>
2025-01-05 18:10:16 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bushdoggo.github.io.',
-	base: '/bushdoggo.com',
 	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'bushdoggo.com';
+export const SITE_DESCRIPTION = 'Welcome to bushdoggo.com!';
+export const GITHUB_URL = 'demo';
+export const EMAIL_ADDRESS = 'demo';
