2394816e2857fac48099a4a35c2cd394e1062e24
bushdoggo <nociwokanto@gmail.com>
2025-01-05 19:31:50 +0900

    修正

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'YABUINU';
-export const SITE_DESCRIPTION = 'Welcome to YABUINU!';
+export const SITE_TITLE = 'bushdoggo.com';
+export const SITE_DESCRIPTION = 'Welcome to bushdoggo.com!';
 export const GITHUB_URL = 'https://github.com/bushdoggo';
 export const EMAIL_ADDRESS = 'demo';
